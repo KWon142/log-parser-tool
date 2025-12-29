6bb327bce81b5f59cf9e7e9584409f3310af97f8
Quan Pham <quan.pk142@gmail.com>
2025-12-29 13:29:54 +0700

    feat: add script test Tinyllama model as an API

---
+++ tests/learning-test-AI/test_ai.py
@@ -0,0 +1,33 @@
+import pytest
+import requests
+import time
+    
+def test_ollama_response():
+    # End point
+    url = "http://localhost:11434/api/generate"
+    # Sample payload for the Ollama API
+    payload = {
+        "model": "tinyllama",
+        "prompt": "Why is the sky blue?",
+        "stream": False
+    }
+
+    start_time = time.time()
+
+    response = requests.post(url, json=payload)
+
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f"Response time: {duration:.2f} seconds")
+    
+    # Assert the HTTP status is 200
+    assert response.status_code == 200, f"Failed! Status code is {response.status_code}"
+    
+    data = response.json()
+    # Assert the response contains a "response" key and (bonus) it is not empty.
+    assert "response" in data, "Failed! Key 'response' not found in JSON"
+    assert len(data["response"]) > 0, "Failed! Response is empty"
+
+    # Assert the response time is under 5 seconds.
+    assert duration <= 5, f"Took {duration}s (Limit is 5s)"
+
